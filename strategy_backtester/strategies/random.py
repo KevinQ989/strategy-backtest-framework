21e929328c8bb3a06f48432d338e5816afdd822a
timoothaii <85624515+timoothaii@users.noreply.github.com>
2026-05-28 08:33:52 +0800

    created backtest engine and a random control strategy (#7)
    * created backtest engine and a random control strategy
    
    * commit gitignore

---
+++ strategy_backtester/strategies/random.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from strategies.base import BaseStrategy
+from core.types import PortfolioWeights
+
+class RandomStrategy(BaseStrategy):
+    def should_rebalance(
+        self,
+        date: pd.Timestamp,
+        last_rebalance: pd.Timestamp,
+        current_weights: pd.Series,
+        prices: pd.DataFrame
+    ) -> bool:
+        return True #rebalance everyday for maximum chaos
+    
+    def generate(
+        self,
+        prices: pd.DataFrame,
+        as_of: pd.Timestamp,
+        current_weights: pd.Series
+    ) -> PortfolioWeights:
+
+        #Get list of unique ticker names
+        tickers = prices.['Ticker'].unique()
+
+        #Generate random weights between -1 (Short) and +1 (Long), for each ticker
+        random_values = np.randomo.uniform(-1,1,size = len(tickers))
+        raw_weights = pd.Series(random_values, index = tickers)
+
+        longs = raw_weights[raw_weights > 0]
+        shorts = raw_weights[raw_weights < 0]
+
+        #Normalise weights so total exposure is 1
+        total_exposure = longs.sum() + shorts.abs().sum()
+
+        if total_exposure > 0:
+            longs = longs/total_exposure
+            shorts = shorts/total_exposure
+
+        return PortfolioWeights(
+            date = as_of,
+            long_weights = longs,
+            short_weights = shorts
+        )
